9c716550222689a6b4f586dc87982c2502787de0
Wlad <wladkrychin@gmail.com>
2021-08-31 20:40:00 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./App.css";
 import Buttons from "./components/Buttons/Buttons";
 import Section from "./components/Section/Section";
 import Statics from "./components/Statics/Statics";
 import Notification from "./components/Notification/Notification";
-class App extends Component {
-  state = {
+function App() {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  handleFeedBack = (option) => {
-    this.setState((state) => ({
+  const handleFeedBack = (option) => {
+    setState((state) => ({
+      ...state,
       [option]: state[option] + 1,
     }));
   };
 
-  totalFeedBack = () => {
-    return Object.values(this.state).reduce((acc, option) => acc + option, 0);
+  const totalFeedBack = () => {
+    return Object.values(state).reduce((acc, option) => acc + option, 0);
   };
-  totalPositiveFeedBack = () => {
-    return Math.round((this.state.good / this.totalFeedBack()) * 100 || 0);
+  const totalPositiveFeedBack = () => {
+    return Math.round((state.good / totalFeedBack()) * 100 || 0);
   };
-  render() {
-    const message = "No feedback given";
-    return (
-      <div className="container">
-        <Section title="Please leave feedback">
-          <div className="buttons__container">
-            <Buttons
-              options={Object.keys(this.state)}
-              feedBack={this.handleFeedBack}
-            />
-          </div>
-        </Section>
-        <Section title="Statistics">
-          {this.totalFeedBack() === 0 ? (
-            <Notification text={message} />
-          ) : (
-            <Statics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              totalFeedback={this.totalFeedBack()}
-              positiveFeedBack={this.totalPositiveFeedBack()}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
+  const message = "No feedback given";
+  return (
+    <div className="container">
+      <Section title="Please leave feedback">
+        <div className="buttons__container">
+          <Buttons options={Object.keys(state)} feedBack={handleFeedBack} />
+        </div>
+      </Section>
+      <Section title="Statistics">
+        {totalFeedBack() === 0 ? (
+          <Notification text={message} />
+        ) : (
+          <Statics
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
+            totalFeedback={totalFeedBack()}
+            positiveFeedBack={totalPositiveFeedBack()}
+          />
+        )}
+      </Section>
+    </div>
+  );
 }
 
 export default App;
